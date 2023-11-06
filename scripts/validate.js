24caab2da8a33f3b896ed0b30f69ad52f686d829
Gustavo Kasai <gukasai@gmail.com>
2023-11-06 18:46:10 -0300

    revisao 4

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,5 +1,16 @@
 
-function enableValidation() {
+function enableValidation({
+  formSelector:"popup__form",
+  inputSelector:"popup__input",
+  submitButtonSelector:"popup__button",
+  inactiveButtonClass:"popup__button_disabled"
+  inputErrorClass: "popup__input_type_error",
+  errorClass: "popup__error_visible",
+  ,
+}
+
+
+) {
   function hideInputError(errorElement) {
     if (!errorElement.classList.contains('disabled')) {
       errorElement.classList.add('hide');
